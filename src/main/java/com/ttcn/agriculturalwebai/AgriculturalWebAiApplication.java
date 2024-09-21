4872979e4ebb824e3b04cf1f0bebc37c63299617
Vũ Văn Hoàng <hoangvuvan677@gmail.com>
2024-09-21 22:08:45 +0700

    First commit

---
+++ src/main/java/com/ttcn/agriculturalwebai/AgriculturalWebAiApplication.java
@@ -0,0 +1,13 @@
+package com.ttcn.agriculturalwebai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriculturalWebAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgriculturalWebAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ttcn/agriculturalwebai/AgriculturalWebAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttcn.agriculturalwebai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriculturalWebAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
