cbad43ddd32308ec5ee61560ef3e5e5685cc5c04
josdinhson2003 <son28gt@gmail.com>
2025-01-07 21:27:08 +0700

    2127

--- src/main/java/com/ttcn/vnuaexam/dto/search/SearchDto.java
+++ src/main/java/com/ttcn/vnuaexam/dto/search/SearchDto.java
@@ -1,27 +1,30 @@
 package com.ttcn.vnuaexam.dto.search;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.data.domain.Sort;
 
 import java.io.Serializable;
 
 @Getter
+@AllArgsConstructor
 public class SearchDto implements Serializable {
     private int pageIndex = 1;
     private int pageSize = 10;
+    private String sortField;
+    private String sortDirection = "ASC";
     private String keyword;
 
     public void setPageIndex(Integer pageIndex) {
         if (pageIndex != null && pageIndex > 0) {
             this.pageIndex = pageIndex;
         }
-
     }
 
     public void setPageSize(Integer pageSize) {
         if (pageSize != null && pageSize > 0) {
             this.pageSize = pageSize;
         }
-
     }
 
     public void setPageIndexAndPageSize(Integer pageIndex, Integer pageSize) {
@@ -41,6 +44,18 @@ public class SearchDto implements Serializable {
     public void setKeyword(final String keyword) {
         if (keyword != null && !keyword.isEmpty()) {
             this.keyword = keyword.trim();
+        }
+    }
+
+    public void setSortField(final String sortField) {
+        if (sortField != null && !sortField.isEmpty()) {
+            this.sortField = sortField.trim();
+        }
+    }
+
+    public void setSortDirection(final String sortDirection) {
+        if (sortDirection != null && !sortDirection.isEmpty()) {
+            this.sortDirection = sortDirection.trim();
         }
     }
 
